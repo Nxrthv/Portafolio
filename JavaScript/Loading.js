b86d01c7bf436b3ca8bd254a5417aa28a0f27f20
Nxrthv <vargasesquivelandres@gmail.com>
2023-09-30 16:41:03 -0500

    Organization

---
+++ JavaScript/Loading.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', () =>{
+    const contenedor_loader = document.querySelector('.content_loader')
+        contenedor_loader.style.opacity = 0
+        contenedor_loader.style.visibility = 'hidden'
+})
