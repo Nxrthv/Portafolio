478322471d3d53206962f39a8b053169161b7c11
AndresVargas0 <1458586@senati.pe>
2024-07-03 00:23:26 -0500

    Update(03/07/24)

--- JavaScript/AnimationTypewriter.js
+++ JavaScript/AnimationTypewriter.js
@@ -1,19 +1,11 @@
-const palabras = ["I'm Student...","Software Engineer...", "Web Developer...", "Front Developer..."];
+const palabras = ["Student...", "Software Engineer...", "Web Developer...", "Front Developer..."];
 const MaquinaDeEscribir = document.getElementById("animation");
 let indicePalabraActual = 0;
-let index = 0;
-let intervalId;
-function escribirPalabra() {
-    MaquinaDeEscribir.innerHTML = palabras[indicePalabraActual].slice(0, index) + '<span class="cursor"></span>';
-    index++
-    if (index > palabras[indicePalabraActual].length) {
-        clearInterval(intervalId);
-        setTimeout(() => {
-            MaquinaDeEscribir.innerHTML = "";
-            indicePalabraActual = (indicePalabraActual + 1) % palabras.length;
-            index = 0;
-            intervalId = setInterval(escribirPalabra, 150);
-        }, 1000);
-    }
+
+function cambiarPalabra() {
+    MaquinaDeEscribir.innerHTML = palabras[indicePalabraActual];
+    indicePalabraActual = (indicePalabraActual + 1) % palabras.length;
+    setTimeout(cambiarPalabra, 4000); // Cambia la palabra cada 2 segundos (2000 ms)
 }
-intervalId = setInterval(escribirPalabra, 150);
+
+cambiarPalabra();
